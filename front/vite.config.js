e55d9292d5e66161a238aadbb7b518913a3d846f
Alexis Godet <66777879+alexlastique@users.noreply.github.com>
2024-04-19 19:24:13 +0200

    clean des erreur

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/get-on-your-apprenticeship/',
-  plugins: [react()]
+  plugins: [react()],
 })
