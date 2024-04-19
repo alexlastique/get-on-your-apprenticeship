49ffe0541ac832698ceb9b5b5354f6cf03e0a529
Alexis Godet <66777879+alexlastique@users.noreply.github.com>
2024-04-19 19:44:50 +0200

    base on vite config

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://alexlastique.github.io/get-on-your-apprenticeship",
 })
