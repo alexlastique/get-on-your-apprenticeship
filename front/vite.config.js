3c38d1651e7abf57aebdb8a61a046bf6224d9698
Alexis Godet <66777879+alexlastique@users.noreply.github.com>
2024-04-20 14:17:44 +0200

    #

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    assetsInlineLimit: 0,
-  },
+  plugins: [react()]
 })
