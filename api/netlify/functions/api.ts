8694fefac5032bdd1ad352195be46ad2d3837b7b
Alexis Godet <66777879+alexlastique@users.noreply.github.com>
2024-04-20 18:24:43 +0200

    fin

--- api/netlify/functions/api.ts
+++ api/netlify/functions/api.ts
@@ -21,6 +21,11 @@ exports.handler = async function (event, context) {
 
     return {
         statusCode: 200,
+        headers:{
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+            },
         body: JSON.stringify(realRouter)
     }
 }
